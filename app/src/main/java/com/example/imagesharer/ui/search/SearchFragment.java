e5daa9ccc711fcd19522a6e7a3df7498ae154175
cjkim <cjkim00@gmail.com>
2022-04-18 23:25:37 -0700

    Initial commit

---
+++ app/src/main/java/com/example/imagesharer/ui/search/SearchFragment.java
@@ -0,0 +1,53 @@
+package com.example.imagesharer.ui.search;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.imagesharer.R;
+import com.example.imagesharer.databinding.FragmentSearchBinding;
+import com.google.android.material.snackbar.Snackbar;
+
+
+public class SearchFragment extends Fragment {
+
+    private FragmentSearchBinding binding;
+    public SearchFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        SearchViewModel homeViewModel =
+                new ViewModelProvider(this).get(SearchViewModel.class);
+
+        binding = FragmentSearchBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        final TextView textView = binding.textSearch;
+        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
